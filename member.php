a2fa1cd8a9cfc03198d4a2a33ff109dbeb4b19ff
wik <fgn.dwikasetiawan@gmail.com>
2015-05-05 22:00:01 +0700

    huehue

--- member.php
+++ member.php
@@ -68,12 +68,11 @@
   <body>
     <div class="navbar">
       <ul class="navbar-item">
-        <li style="background: white; border-bottom: solid #AAAAAA 1px; border-right: none; height: 49px;"><img id="logo" src="logo.png"></li>
+        <li style="background: white; border-bottom: solid #AAAAAA 1px; border-right: none; height: 49px;"><img id="logo" src="logo.png"></li><a href="home"><li>Home</li></a><a href="sertifikat_user.php"><li>Sertifikat Anda</li></a>
       </ul>
     </div>
-    <h1 class="mid" style="margin-top: 50px; margin-bottom: 50px; font-size: 3em">Selamat Datang</h1>
-    <p class="mid" style="font-size: 1.1em">Silahkan buat akun terlebih dahulu untuk dapat meminta sertifikat digital</p><br>
-    <div class="mid"><a href="daftar.php" id="button">Daftar</a></div><br>
-    <p class="mid" style="font-size: 0.8em; color: #555555;">Sudah punya akun? silahkan <a href="login.php">login</a></p>
+    <h1 class="mid" style="margin-top: 50px; margin-bottom: 50px; font-size: 3em">Selamat Datang, Anno</h1>
+    <p class="mid" style="font-size: 1.1em">Anda memliki 2 sertifikat aktif</p><br>
+    <div class="mid"><a href=".php" id="button">Buat Sertifikat Baru</a></div><br>
   </body>
 </html>
